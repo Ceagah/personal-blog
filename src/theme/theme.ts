378cdb5287ce9bc48e5c2d8465a1331c97800410
Carlos Henrique Ceagah <carlosceagah@gmail.com>
2023-01-24 16:05:00 -0300

    feat: create mock files to advertising and posts, to start styling

---
+++ src/theme/theme.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  primary: "#1f77b4",
+  secondary: "#ff7f0e",
+  tertiary: "#2ca02c",
+  light: "#d62728",
+  dark: "#9467bd",
+  success: "#5cb85c",
+  warning: "#f0ad4e",
+};
+export const sizes = {
+  small: 12,
+  medium: 16,
+  large: 20,
+  title: 24,
+  subtitle: 18,
+};
+export const spacing = {
+  small: 8,
+  medium: 12,
+  large: 16,
+};
+
+export const fonts = {
+  primary: "'Poppins', sans-serif",
+  secondary: "'Roboto', sans-serif",
+};
