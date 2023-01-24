99d1c8ed75d60b04e878da04921dc10c396d2d51
Carlos Henrique Ceagah <carlosceagah@gmail.com>
2023-01-24 16:43:01 -0300

    chore: add folders and files structure

---
+++ src/components/Advertising/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Advertising: React.FC = () => {
+  return (
+    <S.Container>
+      <h1>Advertising</h1>
+    </S.Container>
+  );
+};
+
+const S = {
+  Container: styled.div``,
+};
+
+export default Advertising;
---
+++ src/components/Post/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Post: React.FC = () => {
+  return (
+    <S.Container>
+      <h1>Post</h1>
+    </S.Container>
+  );
+};
+
+const S = {
+  Container: styled.div``,
+};
+
+export default Post;
